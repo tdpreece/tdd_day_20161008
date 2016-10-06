3d9aa308f8da74180067b513bf33355d95a94a32
Tim Preece <tdpreece@gmail.com>
2016-10-06 22:43:11 +0100

    Add behave example.

---
+++ test/features/example.feature
@@ -0,0 +1,9 @@
+Feature: Example Feature
+  Just and example to check that Behave is set up OK
+
+  Scenario: First example scenario
+
+  Scenario: Weaker opponent
+    Given the ninja has a third level black-belt
+    When attacked by a samurai
+    Then the ninja should engage the opponent
---
+++ test/features/steps/example_step.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+
+
+@given('the ninja has a third level black-belt')
+def step_impl(context):
+    pass
+
+
+@when('attacked by a samurai')
+def step_impl(context):
+    pass
+
+
+@then('the ninja should engage the opponent')
+def step_impl(context):
+    pass
