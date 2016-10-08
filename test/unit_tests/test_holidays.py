e3099d0ff993679ca0dae5edf43b886ee1ea595e
Tim Preece <tdpreece@gmail.com>
2016-10-08 13:55:13 +0100

    Lunch break.

---
+++ test/unit_tests/test_holidays.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestHoliday(TestCase):
+    def test_when
+
--- test/unit_tests/test_shopping_basket.py
+++ test/unit_tests/test_shopping_basket.py
@@ -1,10 +1,22 @@
 from unittest import TestCase
 
+from parameterizedtestcase import ParameterizedTestMixin
 
-class TestCheckout(TestCase):
+
+class TestCheckout(ParameterizedTestMixin, TestCase):
     def test_no_items_return_zero(self):
-        self.assertEqual(0, total())
+        self.assertEqual(0, total([]))
 
+    @ParameterizedTestMixin.parameterize(
+        ('items', 'expected_total'),
+        [
+            ([{'price': 5, 'quantity': 1}], 5),
+            ([{'price': 5, 'quantity': 2}], 10),
+            ([{'price': 1, 'quantity': 1}, {'price': 1, 'quantity': 1}], 2),
+        ]
+    )
+    def test_total_is_price_times_quanity(self, items, expected_total):
+        self.assertEqual(expected_total, total(items))
 
-def total():
-    return 0
+def total(items):
+    return sum(item['price'] * item['quantity'] for item in items)
